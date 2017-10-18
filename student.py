3129458706758a2c6dc75a07c17586b7fdf41d01
JackStarr91 <starrj@gilmour.org>
2017-10-18 15:04:48 -0400

    gesdsfdfdsfd

--- student.py
+++ student.py
@@ -63,12 +63,13 @@ class Piggy(pigo.Pigo):
 
         counter = 0
         for distance in self.scan:
-            if distance and distance < 200 and not found_something:
+            if distance and distance < 80 and not found_something:
+                counter += 1
                 found_something = True
                 print("Object # %d found, I think" % counter)
-                if distance and distance > 200 and found_something:
-                    found_something = False
-                    counter += 1
+            if distance and distance > 80 and found_something:
+                found_something = False
+
         print("\n----I SEE %d OBJECTS----\n" % counter)
 
 
