4de7f222e71f2a7e985387839c754bac193e49ba
JackStarr91 <starrj@gilmour.org>
2017-11-01 15:17:01 -0400

    removed obstacle count to fix nav

--- student.py
+++ student.py
@@ -56,23 +56,6 @@ class Piggy(pigo.Pigo):
 
 
 
-    # YOU DECIDE: How does your GoPiggy dance?
-   # def obstacle_count(self):
-        """scans and estimates the number of obstacles within sight"""
-   #     for x in range(4):
-    #        self.wide_scan()
-     #       found_something = False
-      #      counter = 0
-       #     for distance in self.scan:
-        #        if distance and distance < 60 and not found_something:
-         #           found_something = True
-        #            counter += 1
-         #         print("Object # %d found, I think" % counter)
-          #      if distance and distance > 60 and found_something:
-           #         found_something = False
-        #    print("\n----I SEE %d OBJECTS----\n" % counter)
-         #   self.encR(7)
-
 
 
 
