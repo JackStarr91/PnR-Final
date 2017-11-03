3b822a50f1c051980107d3ddeec70a2c12e1a8c7
JackStarr91 <starrj@gilmour.org>
2017-11-03 14:36:48 -0400

    Fixed menu issue, added more comments, fixed line 163

--- student.py
+++ student.py
@@ -159,7 +159,7 @@ class Piggy(pigo.Pigo):
                             """look left 2 times and then go"""
 
 
-    def cruise(self):  """used to drive straight when robot finds an opening with the farthest distance between the obstacles"""
+    def cruise(self):
         """drive straight while path is clear"""
         self.fwd()
         while self.dist() > self.SAFE_STOP_DIST:
