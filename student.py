49695d7adbfa9fe90570e468e644f326504ea194
JackStarr91 <starrj@gilmour.org>
2017-11-10 14:25:55 -0500

    improvements for nav

--- student.py
+++ student.py
@@ -1,5 +1,6 @@
 import pigo
 import time  # import just in case students need
+import datetime
 import random
 
 # setup logs
@@ -16,6 +17,7 @@ class Piggy(pigo.Pigo):
     def __init__(self):
         """The robot's constructor: sets variables and runs menu loop"""
         print("I have been instantiated!")
+        self.start_time = datetime.datetime.utcnow()
         # Our servo turns the sensor. What angle of the servo( ) method sets it straight?
         self.MIDPOINT = 83
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
@@ -134,6 +136,9 @@ class Piggy(pigo.Pigo):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
+        right_now = datetime.datetime.utcnow()
+        difference = right_now - self.start_time).seconds
+        print ("It took you &d seconds to run this" % difference)
         while True:
             if self.is_clear():
                 self.cruise()
@@ -147,8 +152,19 @@ class Piggy(pigo.Pigo):
                 self.encB(10)
                 # check right and go right if clear
 
+    def smooth_turn(self):
+        self.right_rot()
+        start = datetime.datetime.utcnow()
+        self.servo(self.MIDPOINT)
+        while True:
+            if self.dist() > 100:
+            self.stop()
+        elif datetime.datetime.utcnow() - start > datetime.timedelta(seconds=10):
+            self.stop()
+        time.sleep(.2)
+
     def cruise(self):
-        #drive straight while path is clear"""
+        """drive straight while path is clear"""
         self.fwd()
         while self.dist() > self.SAFE_STOP_DIST:
             time.sleep(.5)
