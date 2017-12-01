42f52842e7413e89bcbd70d0a5132417027a9577
JackStarr91 <starrj@gilmour.org>
2017-12-01 14:30:42 -0500

    lowered if count

--- student.py
+++ student.py
@@ -156,7 +156,7 @@ class Piggy(pigo.Pigo):
             else:
                 self.switch_turn(9)
                 count += 1
-                if count > 4:
+                if count > 2:
                     self.switch_turn(10)
 
 
